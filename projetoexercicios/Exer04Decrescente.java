82eb8380f3fdb97afc3953ea9ad4e805fdb6df59
AdemirJr225 <101716954+AdemirJr225@users.noreply.github.com>
2022-06-15 13:33:52 -0300

    Delete projetoexercicios directory

--- projetoexercicios/Exer04Decrescente.java
+++
@@ -1,5 +0,0 @@
-package projetoexercicios;
-import java.util.*;
-public class Exer4Decrescente {
-
-}
--- projetoexercicios/Exer22Entre100.java
+++
@@ -1,9 +0,0 @@
-package projetoexercicios;
-import java.util.*;
-public class Exer22Entre100 {
-	public static void main(String[] args) {
-	}
-	}
-	
-		
-
